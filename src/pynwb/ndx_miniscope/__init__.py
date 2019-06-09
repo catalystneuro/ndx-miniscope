5dfd207d474a088cbe47af3fe92fafb1cb713ac3
bendichter <ben.dichter@gmail.com>
2019-06-09 15:30:52 -0700

    auto-gen class

---
+++ src/pynwb/ndx_miniscope/__init__.py
@@ -0,0 +1,12 @@
+import os
+from pynwb import load_namespaces, get_class
+from os import path
+
+name = 'miniscope'
+
+here = path.abspath(path.dirname(__file__))
+ns_path = os.path.join(here, 'spec', name + '.namespace.yaml')
+
+load_namespaces(ns_path)
+
+Miniscope = get_class('Miniscope', name)
