4cc676a21edb9ba9de4aec27e8e9d4ee3b56ca3e
Ben Dichter <ben.dichter@gmail.com>
2019-06-26 16:45:23 -0400

    update export_spec

--- src/spec/export_spec.py
+++ src/spec/export_spec.py
@@ -1,11 +1,10 @@
 import os
-
+import warnings
 
 def export_spec(ns_builder, new_data_types):
     """
     Creates YAML specification files for a new namespace and extensions with
     the given new neurodata types.
-
     Args:
         ns_builder - pynwb.spec.NWBNamespaceBuilder instance used to build the
                      namespace and extension
@@ -13,22 +12,26 @@ def export_spec(ns_builder, new_data_types):
                          to be added
     """
 
-    if 'name' not in ns_builder._NamespaceBuilder__ns_args:
+    if len(new_data_types) == 0:
+        warnings.warn('No data types specified. Exiting.')
+        return
+
+    if ns_builder.name is None:
         raise RuntimeError('Namespace name is required to export specs')
 
     project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
     output_dir = os.path.join(project_dir, 'spec')
 
-    ns_path = ns_builder._NamespaceBuilder__ns_args['name'] + '.namespace.yaml'
-    ext_path = ns_builder._NamespaceBuilder__ns_args['name'] + '.extensions.yaml'
-    
+    ns_path = ns_builder.name + '.namespace.yaml'
+    ext_path = ns_builder.name + '.extensions.yaml'
+
     if len(new_data_types) > 1:
         pluralize = 's'
     else:
         pluralize = ''
 
     print('Creating file {output_dir}/{ext_path} with {new_data_types_count} data type{pluralize}'.format(
-        pluralize=pluralize, output_dir=output_dir, ext_path=ext_path, 
+        pluralize=pluralize, output_dir=output_dir, ext_path=ext_path,
         new_data_types_count=len(new_data_types)))
 
     for neurodata_type in new_data_types:
